148e7e5502fca4ea9bdc6996c8e6d2d45ff19a86
CN-JS-HuiBai <h208434317@gmail.com>
2023-12-05 20:39:08 +0800

    fix some problems

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 make mrproper
-cp rkbin / -r
-export ROCKCHIP_TPL=/rkbin/rk35/rk3566_ddr_1056MHz_v1.10.bin
-export BL31=/rkbin/rk35/rk3568_bl31_v1.32.elf
+cp rkbin /tmp -r
+export ROCKCHIP_TPL=/tmp/rkbin/rk35/rk3566_ddr_1056MHz_v1.10.bin
+export BL31=/tmp/rkbin/rk35/rk3568_bl31_v1.32.elf
 make CROSS_COMPILE=aarch64-linux-gnu- orangepi-3b-rk3566_defconfig
 make CROSS_COMPILE=aarch64-linux-gnu- all -j4
-rm -rf /rkbin
+rm -rf /tmp/rkbin
