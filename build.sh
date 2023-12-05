ea7b17ef2a60a1ebfc012e616279f5954fefb6a9
CN-JS-HuiBai <h208434317@gmail.com>
2023-12-05 18:07:53 +0800

    fix build rkbin

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #compile-uboot
 make mrproper
-export ROCKCHIP_TPL=/root/rkbin/bin/rk35/rk3566_ddr_1056MHz_v1.18.bin
-export BL31=/root/rkbin/bin/rk35/rk3568_bl31_ultra_v2.13.elf
-export TEE=/root/optee_os/out/arm-plat-vexpress/core/tee.bin
+export ROCKCHIP_TPL=../rkbin/bin/rk35/rk3566_ddr_1056MHz_v1.18.bin
+export BL31=../rkbin/bin/rk35/rk3568_bl31_ultra_v2.13.elf
+#export TEE=/root/optee_os/out/arm-plat-vexpress/core/tee.bin
 make CROSS_COMPILE=aarch64-linux-gnu- orangepi-3b-rk3566_defconfig
 make CROSS_COMPILE=aarch64-linux-gnu- all -j4
-
