da492355ba71200bc699ea8c03b4046f3279c47c
CN-JS-HuiBai <h208434317@gmail.com>
2023-12-05 18:20:39 +0800

    fix build problem

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
-#compile-uboot
 make mrproper
-export ROCKCHIP_TPL=rkbin/bin/rk35/rk3566_ddr_1056MHz_v1.18.bin
-export BL31=rkbin/bin/rk35/rk3568_bl31_ultra_v2.13.elf
-#export TEE=/root/optee_os/out/arm-plat-vexpress/core/tee.bin
+export ROCKCHIP_TPL=/root/armbian-rkbin/rk35/rk3566_ddr_1056MHz_v1.10.bin
+export BL31=/root/armbian-rkbin/rk35/rk3568_bl31_v1.32.elf
 make CROSS_COMPILE=aarch64-linux-gnu- orangepi-3b-rk3566_defconfig
 make CROSS_COMPILE=aarch64-linux-gnu- all -j4
